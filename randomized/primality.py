9cf8e4a7d06dda89b5e0a6ecfd8013d4b368ecdd
Jeremy Kun <kun.jeremy@gmail.com>
2016-07-09 12:55:54 -0700

    randomized/primality: fixed python 2-to-3 bug, added examples

--- randomized/primality.py
+++ randomized/primality.py
@@ -5,23 +5,18 @@ def decompose(n):
     exponentOfTwo = 0
 
     while n % 2 == 0:
-        n = n/2
+        n = n // 2  # using / turns large numbers into floats
         exponentOfTwo += 1
 
     return exponentOfTwo, n
 
 
 def isWitness(possibleWitness, p, exponent, remainder):
-    possibleWitness = pow(possibleWitness, remainder, p)
-
-    if possibleWitness == 1 or possibleWitness == p - 1:
+    if pow(possibleWitness, remainder, p) == 1:
         return False
 
-    for _ in range(exponent):
-        possibleWitness = pow(possibleWitness, 2, p)
-
-        if possibleWitness == p - 1:
-            return False
+    if any(pow(possibleWitness, 2**i * remainder, p) == p - 1 for i in range(exponent)):
+        return False
 
     return True
 
@@ -29,7 +24,7 @@ def isWitness(possibleWitness, p, exponent, remainder):
 def probablyPrime(p, accuracy=100):
     if p == 2 or p == 3:
         return True
-    if p < 2:
+    if p < 2 or p % 2 == 0:
         return False
 
     exponent, remainder = decompose(p - 1)
@@ -40,3 +35,12 @@ def probablyPrime(p, accuracy=100):
             return False
 
     return True
+
+
+if __name__ == "__main__":
+    n = 1
+
+    while not probablyPrime(n, accuracy=100):
+        n = random.getrandbits(512)
+
+    print("{} is prime".format(n))
