3be5bbed079123d41fabaa4e30f99b709e7298b8
Jeremy Kun <j2kun@users.noreply.github.com>
2022-11-15 10:29:58 -0800

    add poly_mul

---
+++ lists_and_sequences/poly_mul.py
@@ -0,0 +1,29 @@
+import numpy
+from numpy.fft import fft, ifft
+
+
+def poly_mul(p1, p2):
+    """Multiply two polynomials.
+
+    p1 and p2 are numpy arrays of coefficients in degree-increasing order.
+    """
+    deg = p1.shape[0] - 1
+    # would be 2*deg + 1, but next power of 2 handles the +1
+    total_num_pts = 2 * deg
+    next_power_of_2 = 1 << (total_num_pts - 1).bit_length()
+
+    ff_p1 = fft(numpy.pad(p1, (0, next_power_of_2 - p1.shape[0])))
+    ff_p2 = fft(numpy.pad(p2, (0, next_power_of_2 - p2.shape[0])))
+    product = ff_p1 * ff_p2
+    return numpy.round(numpy.absolute(ifft(product))).astype(numpy.int32)
+
+
+if __name__ == "__main__":
+    p1 = numpy.array([1, 2, 3, 4], dtype=numpy.int32)
+    p2 = numpy.array([2, 3, 4, 5], dtype=numpy.int32)
+
+    output = poly_mul(p1, p2)
+    expected = numpy.array([2, 7, 16, 30, 34, 31, 20, 0])
+
+    print(f"output=\t\t{output}")
+    print(f"expected=\t{expected}")
